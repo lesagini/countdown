962f8ccf5186d1f53c9afdd165a466776aca24fa
lesagini <emmanuelsagini2@gmail.com>
2022-01-12 12:16:13 +0300

    Added countdown

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+const launchTime = new Date('12/29/2022 10:00')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const currentTime = new Date().getTime()
+  const numberOfMilliseconds = parseInt(launchTime-currentTime)
+  res.render('index', { title: 'Extavaganza', countDown: numberOfMilliseconds });
 });
 
 module.exports = router;
